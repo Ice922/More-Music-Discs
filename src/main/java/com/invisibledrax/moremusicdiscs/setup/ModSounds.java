9d64007580d397ed35a537165c36b65f0aa6d4fc
Ice922 <isaiaheaston922@gmail.com>
2021-07-27 09:37:31 -0500

    Update ModSounds.java

--- src/main/java/com/invisibledrax/moremusicdiscs/setup/ModSounds.java
+++ src/main/java/com/invisibledrax/moremusicdiscs/setup/ModSounds.java
@@ -12,6 +12,5 @@ public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MoreMusicDiscs.MOD_ID);
 
     public static final RegistryObject<SoundEvent> LOKI_THEME_DISC =
-            SOUNDS.register("loki_green_theme", () ->
-                    new SoundEvent(new ResourceLocation(MoreMusicDiscs.MOD_ID, "loki_green_theme")));
+            SOUNDS.register("loki_green_theme", () -> new SoundEvent(new ResourceLocation(MoreMusicDiscs.MOD_ID, "disc/loki_green_theme")));
 }
